a139ca0eef6fcb6fb86abbdfab4dcd1d61bf5b61
Cory Hogan <chogan314@users.noreply.github.com>
2017-10-11 19:15:03 -0500

    changed index div selection from class based to element based

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#index li>ul').parent().find(".index-entry").click(function() {
+    $('#index li>ul').parent().find("div:first-child").click(function() {
         if ($(this).parent().hasClass("show-children")) {
             $(this).parent().removeClass("show-children");
             $(this).parent().find("li").removeClass("show-children");
